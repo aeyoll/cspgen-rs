8b39c99cc48cfcd800dd2b2e3c42b5ecb918d4f6
Jean-Philippe Bidegain <jp@bidega.in>
2020-10-03 19:44:22 +0200

    Improve CSP fmt::Display readability

--- src/csp.rs
+++ src/csp.rs
@@ -11,6 +11,21 @@ pub struct CSP {
 
 impl fmt::Display for CSP {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' {}; font-src 'self' data: {}; img-src 'self' data: {}; style-src 'self' data: 'unsafe-inline' {}; connect-src 'self' {}; iframe-src 'self' {};", self.javascripts.join(" "), self.fonts.join(" "), self.images.join(" "), self.styles.join(" "), self.connects.join(" "), self.iframes.join(" "))
+        write!(
+            f,
+            "Content-Security-Policy: \
+            default-src 'self';\
+            script-src 'self' 'unsafe-inline' 'unsafe-eval' {};\
+            font-src 'self' data: {};\
+            img-src 'self' data: {};\
+            style-src 'self' data: 'unsafe-inline' {};\
+            connect-src 'self' {}; iframe-src 'self' {};",
+            self.javascripts.join(" "),
+            self.fonts.join(" "),
+            self.images.join(" "),
+            self.styles.join(" "),
+            self.connects.join(" "),
+            self.iframes.join(" ")
+        )
     }
 }
